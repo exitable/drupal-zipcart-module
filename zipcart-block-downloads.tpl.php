746e29819e6f74f2e7f80c4f4f205feef277a9d9
Chris Burgess <chris@giantrobot.co.nz>
2010-10-24 23:15:32 +0000

    [#951510] UI improvements; adds JS to cue user from clicked element to cart download process.

--- zipcart-block-downloads.tpl.php
+++ zipcart-block-downloads.tpl.php
@@ -1,8 +1,14 @@
 <?php
 /**
  * Template to display My Downloads link.
+ *
+ * $count - number of files in cart currently
+ * $files - the files
  */
+if ( !$count ) {
+  $count = '0' ;
+}
  ?>
 <div class="zipcart-block-downloads">
-  <?php print l('My Downloads', 'zipcart/get', array('query' => _zipcart_get_destination_alias())) ; ?>
+  <?php print l(t('My Downloads (<span class="zipcart-download-count">!count</span> files)', array('!count' => $count)), 'zipcart/get', array('html' => TRUE, 'query' => _zipcart_get_destination_alias())) ; ?>
 </div>
--- zipcart.js
+++ zipcart.js
@@ -10,9 +10,31 @@ Drupal.behaviors.zipcart = function() {
         filePath = filePath.replace(/^\/+/, '/');
         $.ajax({
           'url': filePath,
+          'dataType': 'json',
           success: function(data, textStatus, req) {
-            // 
-            // console.log(data);
+            Drupal.settings.zipcart.cart = data.cart ;
+            slide = $(e.target).clone();
+            cart = $('.zipcart-block-downloads');
+            // generate a slide-able copy of whatever was clicked.
+            // it would be really nice if this could be attached to 
+            // a related image, eg thumbnail version of the photo
+            // instead of the 'download' link text ...
+            slide.hide();
+            slide.css({ position: 'absolute', top: $(e.target).offset().top, left: $(e.target).offset().left, opacity: 0.75 });
+            slide.appendTo($('body'));
+            slide.show();
+            animProps = {
+              position: 'absolute', 
+              top:  cart.offset().top  + ( ( $(cart).height() - $(e.target).height()/2 ) / 2 ), 
+              left: cart.offset().left + ( ( $(cart).width()  - $(e.target).width()/2  ) / 2 ), 
+              width: $(slide).width()/2, 
+              height: $(slide).height/2 
+            };
+            animCallback = function(data) {
+              $(slide).hide() ;
+              $('.zipcart-download-count').html( Drupal.settings.zipcart.cart.length ) ;
+            }
+            slide.animate(animProps, 'slow', 'swing', animCallback);
           },
           error: function(req, textStatus, errorThrown) {
             alert('Unable to add the file to your ZipCart.');
