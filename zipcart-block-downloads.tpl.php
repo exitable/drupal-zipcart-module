18c8de2ed59d9a66a429c6d7fcf107f4d361127e
Chris Burgess <chris@giantrobot.co.nz>
2010-10-25 21:40:26 +0000

    Show number of files in zipcart-block-downloads.tpl.php.

--- zipcart-block-downloads.tpl.php
+++ zipcart-block-downloads.tpl.php
@@ -5,9 +5,6 @@
  * $count - number of files in cart currently
  * $files - the files
  */
-if ( !$count ) {
-  $count = '0' ;
-}
  ?>
 <div class="zipcart-block-downloads">
   <?php print l(t('My Downloads (<span class="zipcart-download-count">!count</span> files)', array('!count' => $count)), 'zipcart/get', array('html' => TRUE, 'query' => _zipcart_get_destination_alias())) ; ?>
