1f0294e6a0ed0f5c52e3209d129b96fdc5e6d29c
Chris Burgess <chris@giantrobot.co.nz>
2012-08-21 11:31:51 +1200

    Issue #1687792. Coding standards.

--- zipcart-block-downloads.tpl.php
+++ zipcart-block-downloads.tpl.php
@@ -8,5 +8,5 @@
  */
  ?>
 <div class="zipcart-block-downloads">
-  <?php print l(t('My Downloads (<span class="zipcart-download-count">!count</span> files)', array('!count' => $count)), 'zipcart/get', array('html' => TRUE, 'query' => _zipcart_get_destination_alias())); ?>
+  <?php print l(t('My Downloads (<span class="zipcart-download-count">!count</span> files)', array('!count' => $count)), ZIPCART_PATH_GET, array('html' => TRUE, 'query' => _zipcart_get_destination_alias())); ?>
 </div>
